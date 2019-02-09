49cb6f4e16396e4b84e319bbd873c7f69aabe267
André Lashley <andre.lashley@gmail.com>
2019-02-08 19:29:43 -0800

    set up debugging

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
-
+var chalk = require('chalk');
+var morgan = require('morgan');
 var app = express();
+
+app.use(morgan('tiny'));
 
 app.get('/', function(req, res) {
     res.send('Hello from my library app');
 });
 
 app.listen(3000, function() {
-    console.log('listening on port 3000');
+    console.log(`listening on port ${chalk.green('3000')}`);
 });
